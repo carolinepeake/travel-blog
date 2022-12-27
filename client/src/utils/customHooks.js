01286413ee02bad70f3667135d29dda78679e16e
Caroline Peake <peake.caroline@gmail.com>
2022-12-26 22:19:55 -0500

    create useCloudinary custom hook

--- client/src/utils/customHooks.js
+++ client/src/utils/customHooks.js
@@ -8,16 +8,58 @@ export const useMapboxApi = () => {
   // custom fetch function consumes location
   const locationFetch = async (searchTerm, locality = 'all') => {
 
-    if (!searchTerm) return;
+    if (!searchTerm) {
+      setAutocompleteLocations([]);
+      return;
+    }
 
     const res = await axios.get(`/locations/places/${searchTerm}/${locality}`);
     console.log('response from handleLocationChange api call:', res);
-    !autocompleteLocations.includes(searchTerm) &&
+    // for loop is quicker than map
+    !autocompleteLocations.map(location => location.place_name).includes(searchTerm) &&
       res.data.features &&
-      setAutocompleteLocations(res.data.features.map((place) => place.place_name));
+      setAutocompleteLocations(res.data.features.map(({ place_name, context, place_type, id }) => ({ place_name, context, place_type, id })));
       console.log('autocompleteLocations: ', autocompleteLocations);
     res.error ? setAutocompleteErr(res.error) : setAutocompleteErr("");
   };
 
   return [autocompleteLocations, autocompleteErr, locationFetch]; // <-- return state and fetch function
+};
+
+// export const useFilterPosts = async (route, filterTerm, e) => {
+//     e.preventDefault();
+//     try {
+//       const response = await axios.get(`http://localhost:3001/posts/filter/${route}/${filterTerm}`);
+//       let filteredPosts = response.data;
+//       console.log('response from handleFilterPosts in App component', response.data);
+//       setPosts(filteredPosts);
+//     } catch (err) {
+//       console.log('error filtering posts', err);
+//     }
+// };
+
+export const useCloudinary = () => {
+  const [cloudinaryImage, setCloudinaryImage] = useState({});
+  const [cloudinaryErr, setCloudinaryErr] = useState('');
+
+  const uploadImage = async (fileUrl) => {
+    if (!fileUrl) {
+      setCloudinaryImage('');
+      return;
+    }
+
+    try {
+      const savedImage = await axios.post('http://localhost:3001/posts/cloudinary/upload', {
+        image: fileUrl,
+      });
+      console.log('saved image: ', savedImage);
+      setCloudinaryImage(savedImage.data)
+    } catch (err) {
+      console.log('error uploading cloudinary image: ', err);
+      // set to err.message or whatever it is
+      setCloudinaryErr('error uploading image, file must be less than 64 mb');
+    }
+  }
+
+  return [cloudinaryImage, cloudinaryErr, uploadImage];
 };
